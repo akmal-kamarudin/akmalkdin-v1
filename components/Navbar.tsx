64a98352b46aa93315ccdf799140de2ac791f401
akmal-kamarudin <akmal.kamarudin19@gmail.com>
2023-06-17 17:02:06 +0800

    build: installed daisyUI | style: design navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -37,7 +37,52 @@ const Navbar = () => {
   const currentTheme = theme === "system" ? systemTheme : theme;
   const [navbar, setNavbar] = useState(false);
 
-  return <div>Navbar</div>;
+  return (
+    <header data-theme="light">
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">Akmal Kamarudin</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            {NavItem.map((item, id) => {
+              return (
+                <li>
+                  <a key={id}>{item.label}</a>
+                </li>
+              );
+            })}
+
+            <li>
+              <details>
+                <summary>Theme</summary>
+                <ul className="p-2 bg-base-100">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+
+            <li>
+              {currentTheme === "dark" ? (
+                <button>
+                  <RiSunLine />
+                </button>
+              ) : (
+                <button>
+                  <RiMoonClearLine />
+                </button>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
