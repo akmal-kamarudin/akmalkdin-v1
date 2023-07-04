fed4d15e40a7882f44e29e097bcc47f84aded56b
akmal-kamarudin <akmal.kamarudin19@gmail.com>
2023-07-04 13:30:38 +0800

    feat: motion for more items

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,6 +1,26 @@
 import React from "react";
+import experience from "@/lib/ExpList";
+import MotionItems from "./MotionItems";
 
 const Experience = () => {
+  const renderExperience = experience.map((items, index) => {
+    const isLastItem = index === experience.length - 1;
+
+    return (
+      <MotionItems key={index} itemKey={index}>
+        <div
+          className="grid h-32 card bg-base-200 rounded-box place-items-center opacity-90"
+          data-theme="light"
+        >
+          <h1>{items.job}</h1>
+          <h3>{items.location}</h3>
+          <p>{items.period}</p>
+        </div>
+        {!isLastItem && <div className="divider text-zinc-300">|</div>}
+      </MotionItems>
+    );
+  });
+
   return (
     <div className="flex flex-col items-start text-justify justify-start pt-16 w-full">
       <h1 className="font-semibold text-xl lg:text-2xl">Experiences</h1>
@@ -9,34 +29,7 @@ const Experience = () => {
         remarkable companies that have provided me with valuable experiences.
       </p>
       <div className="flex flex-col w-full items-center justify-center text-center">
-        <div className="w-full py-8 md:w-3/5">
-          <div
-            className="grid h-32 card bg-base-200 rounded-box place-items-center opacity-90"
-            data-theme="light"
-          >
-            <h1>Junior Programmer @ Saf Pilihan</h1>
-            <h3>Bandar Baru Bangi, Malaysia </h3>
-            <p>Jan 2023 - Present</p>
-          </div>
-          <div className="divider text-zinc-300">|</div>
-          <div
-            className="grid h-32 card bg-base-200 rounded-box place-items-center opacity-90"
-            data-theme="light"
-          >
-            <h1>Biomedical Engineer @ Altica</h1>
-            <h3>Puchong, Malaysia </h3>
-            <p>Jul 2021 - Feb 2023</p>
-          </div>
-          <div className="divider text-zinc-300">|</div>
-          <div
-            className="grid h-32 card bg-base-200 rounded-box place-items-center opacity-90"
-            data-theme="light"
-          >
-            <h1>Intern Junior Biomedical Engineer @ Advance Pact</h1>
-            <h3>Johor Bahru, Malaysia </h3>
-            <p>Jan 2018 — May 2018</p>
-          </div>
-        </div>
+        <div className="w-full py-8 md:w-3/5">{renderExperience}</div>
       </div>
     </div>
   );
