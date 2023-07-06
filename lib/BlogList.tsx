1f3efc49bbd563e43d308161c36461c603646a4a
akmal-kamarudin <akmal.kamarudin19@gmail.com>
2023-07-07 03:35:30 +0800

    style: blog posts card

--- lib/BlogList.tsx
+++ lib/BlogList.tsx
@@ -10,30 +10,23 @@ export type BlogObj = {
 
 const BlogList: BlogObj[] = [
   {
-    title: "Junior Programmer @ Saf Pilihan",
+    title: "Junior Programmer @ Saf Pilihan 1",
     date: "19/12/2022",
     pic: "Jan 2023 - Present",
     desc: "Each project that I'm involved on represents a unique challenge and an opportunity for me to learn and grow.",
     url: "https://akmalkdin-v1.vercel.app/",
   },
   {
-    title: "Intern Junior Biomedical Engineer @ Altica",
+    title: "Intern Junior Biomedical Engineer @ Altica 2",
     date: "19/12/2022",
     pic: "Jul 2021 - Feb 2023",
     desc: "Each project that I'm involved on represents a unique challenge and an opportunity for me to learn and grow.",
     url: "https://akmalkdin-v1.vercel.app/",
   },
   {
-    title: "Intern Junior Biomedical Engineer @ Advance Pact",
+    title: "Intern Junior Biomedical Engineer @ Advance Pact 3",
     date: "19/12/2022",
     pic: "Jan 2018 — May 2018",
-    desc: "Each project that I'm involved on represents a unique challenge and an opportunity for me to learn and grow.",
-    url: "https://akmalkdin-v1.vercel.app/",
-  },
-  {
-    title: "Junior Programmer @ Saf Pilihan",
-    date: "19/12/2022",
-    pic: "Jan 2023 - Present",
     desc: "Each project that I'm involved on represents a unique challenge and an opportunity for me to learn and grow.",
     url: "https://akmalkdin-v1.vercel.app/",
   },
