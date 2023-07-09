1074e163bf7ed9b3f4cb784203e784d1352632fa
akmal-kamarudin <akmal.kamarudin19@gmail.com>
2023-07-09 22:35:48 +0800

    chore: blog api

--- app/api/posts/BlogApi.ts
+++ app/api/posts/BlogApi.ts
@@ -1,36 +1,36 @@
-import axios from "axios";
+// import axios from "axios";
 
-const endpoint = "https://api.hashnode.com/";
-const ARTICLE_QUERY = `
-  {
-    user(username: "captain-eo") {
-      publication {
-        posts(page: 0) {
-          title
-          coverImage
-          dateUpdated
-          brief
-          slug
-        }
-      }
-    }
-  }
-`;
+// const endpoint = "https://api.hashnode.com/";
+// const ARTICLE_QUERY = `
+//   {
+//     user(username: "captain-eo") {
+//       publication {
+//         posts(page: 0) {
+//           title
+//           coverImage
+//           dateUpdated
+//           brief
+//           slug
+//         }
+//       }
+//     }
+//   }
+// `;
 
-const uploadImage = async () => {
-  try {
-    const response = await axios.post(`${endpoint}`, {
-      params: {
-        // key: apiKey,
-        // expiration: 2592000,
-      },
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
+// const uploadImage = async () => {
+//   try {
+//     const response = await axios.post(`${endpoint}`, {
+//       params: {
+//         // key: apiKey,
+//         // expiration: 2592000,
+//       },
+//       headers: {
+//         "Content-Type": "multipart/form-data",
+//       },
+//     });
 
-    return response.data.data.url;
-  } catch (error) {
-    console.error(error);
-  }
-};
+//     return response.data.data.url;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
